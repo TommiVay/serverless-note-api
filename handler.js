1ff1653c8fdb7e1e0345a76b7036024d92d3ebc4
Tommi Vayrynen <vayrynen.tommi1996@gmail.com>
2020-08-29 13:35:53 +0300

    first commit

---
+++ handler.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+export const hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Go Serverless v1.0! ${await message({
+        time: 1,
+        copy: "Your function executed successfully!",
+      })}`,
+    }),
+  };
+};
+
+const message = ({ time, ...rest }) =>
+  // eslint-disable-next-line no-unused-vars
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      resolve(`${rest.copy} (with a delay)`);
+    }, time * 1000)
+  );
